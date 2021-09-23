5fa4a5b1e4c4ec45421496e5804741e062cf4b37
kebaldwin <90184259+kebaldwin@users.noreply.github.com>
2021-09-23 15:39:41 +1000

    Add displayForecast function

--- src/app.js
+++ src/app.js
@@ -14,6 +14,46 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response){
+    console.log(response.data.daily);
+    let forecastElement=document.querySelector("#forecast");
+    
+    let forecastHTML=`<div class ="row">`;
+    let days =["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+    days.forEach(function(day){
+    forecastHTML=
+        forecastHTML + 
+        `
+        <div class="col-2">
+            <div class="weather-forecast-day">${day}</div>
+                <img class ="weather-forecast-img"
+                    src ="https://openweathermap.org/img/wn/50d@2x.png"
+                    alt =""
+                    width="36"
+                /> 
+            <div class ="weather-forecast-temp">
+                <span class = "weather-forecast-temp-max">
+                    18°
+                </span>
+                <span class ="weather-forecast-temp-min">
+                    12°
+                </span>
+            </div>   
+        </div>
+        `;
+    });
+
+    forecastHTML=forecastHTML+`</div>`;
+    forecastElement.innerHTML=forecastHTML;
+}
+
+function getForecast(coordinates){
+    console.log(coordinates);
+    let apiKey="c4cd19b1bf4fcc9bdffc7f2ac1741ca9";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -32,6 +72,8 @@ function displayTemperature(response) {
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord);
 }
 
 function search(city){
